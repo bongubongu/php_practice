0f719852db55460cae12005c6c015635532c6d8e
”名前” <>
2019-07-23 05:21:03 +0000

    php.Laravel.03.課題

--- practice.php
+++ practice.php
@@ -1,21 +1,26 @@
 <?php
 echo 'hello php!';
+echo "\n";
 
 $a = 3;
 $b = 7;
 echo $a + $b;
+echo "\n";
 
 $array_month = ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'];
 echo $array_month[7];
+echo "\n";
 
 $hello = "Hello,";
 $name = "柿田";
 $world = "s World";
 echo $hello.$name.$world;
+echo "\n";
 
 $tech_boost = "tech";
 $tech_boost .= " boost";
 echo $tech_boost;
+echo "\n";
 
 $calender_2018 = [
   "January" => "1月",
@@ -32,5 +37,5 @@ $calender_2018 = [
   "December" => "12月"
 ];
 echo $calender_2018 ["December"];
-
+echo "\n";
 
